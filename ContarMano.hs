e28f4012ad0d4ba5d0f99b80cf2ef6f1e0b9ad8f
julian9809 <juliand9809@gmail.com>
2017-09-18 16:12:08 -0500

    Add files via upload

--- ContarMano.hs
+++ ContarMano.hs
@@ -1,6 +1,6 @@
-contarMano::[(Char,Char)]->Int 
+contarMano::[(Char,Char)]->Int
 contarMano [ ] = 0
-contarMano ((a,b):xs) 
+contarMano ((a,b):xs)
  | a == 'A' = 1 + contarMano(xs)
  | a == '2' = 2 + contarMano(xs)
  | a == '3' = 3 + contarMano(xs)
@@ -15,3 +15,15 @@ contarMano ((a,b):xs)
  | a == 'Q' = 10 + contarMano(xs)
  | otherwise = 0 + contarMano(xs)
 
+buscarAs::[(Char,Char)]->Bool
+buscarAs [ ] = False
+buscarAs ((a,b):xs)
+ | a == 'A' = True
+ | a /= 'A' = buscarAs xs
+
+contarAs::[(Char,Char)]->Int
+contarAs [] = 0
+contarAs ((a,b):xs)
+ | (buscarAs ((a,b):xs) == True && contarMano ((a,b):xs) > 11 ) = contarMano ((a,b):xs)
+ | (buscarAs ((a,b):xs) == True && contarMano ((a,b):xs) <= 11 ) = contarMano ((a,b):xs) + 10
+ | otherwise = contarMano ((a,b):xs)
